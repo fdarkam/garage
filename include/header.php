16c31fed2c4fc96f3d05789ec25de20580433a52
Darkam Fabroce <darkamfabrice@gmail.com>
2023-09-29 09:51:03 +1100

    version final

--- include/header.php
+++ include/header.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Garage piaud</title>
+  <title>Garage SIO</title>
   <link rel="stylesheet" href="../css/style.css">
 </head>
 
